f076e97061c67072115f456c7179f889d6d4c9ff
kris23lis <kris23lis@gmail.com>
2021-06-03 13:22:59 +0300

    Добавила методы которые по идее должны разбирать данные из массива json

--- app/src/main/java/com/example/filmsapp/domain/Repository.kt
+++ app/src/main/java/com/example/filmsapp/domain/Repository.kt
@@ -7,10 +7,10 @@ interface Repository {
 
     fun  getFilm(executor : Executor, callback: (result : RepositoryResult<Film>) -> Unit)
     fun  getCategory(executor : Executor, callback: (result : RepositoryResult<List<Category>>) -> Unit)
-    fun  getPopular() : Array<Film>
-    fun  getNowPlaying() : Array<Film>
-    fun  getTopRated() : Array<Film>
-    fun  getUpcoming() : Array<Film>
+    fun  getPopular() : ArrayList<Film>
+    fun  getNowPlaying() : ArrayList<Film>
+    fun  getTopRated() : ArrayList<Film>
+    fun  getUpcoming() : ArrayList<Film>
 
 
 
